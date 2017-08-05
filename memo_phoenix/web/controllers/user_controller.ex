5e6f794b96cbee5828eaa698366ac21e480f88c2
stlange <ststlange@gmail.com>
2017-08-06 08:38:01 +0900

    add action: post /users

--- memo_phoenix/web/controllers/user_controller.ex
+++ memo_phoenix/web/controllers/user_controller.ex
@@ -16,4 +16,17 @@ defmodule MemoPhoenix.UserController do
   def show(conn, %{"user" => user}) do
     render conn, "show.html", user: user
   end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(changeset) do 
+      {:ok, user} -> 
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
 end
