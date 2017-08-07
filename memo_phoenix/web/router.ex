c88b42843f0dbc41d0b4ef7f499d723988ea490e
stlange <ststlange@gmail.com>
2017-08-08 06:03:54 +0900

    add auth module

--- memo_phoenix/web/router.ex
+++ memo_phoenix/web/router.ex
@@ -7,6 +7,7 @@ defmodule MemoPhoenix.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug MemoPhoenix.Auth, repo: MemoPhoenix.Repo
   end
 
   pipeline :api do
